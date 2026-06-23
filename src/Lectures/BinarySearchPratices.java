a344dc84a12dea63fe5a80cb7ed77e1134df8064
Sahil Kumar Chaudhary <sahilku707@gmail.com>
2026-06-23 15:06:27 +0530

    Solving Binary Search Problems2

--- src/Lectures/BinarySearchPratices.java
+++ src/Lectures/BinarySearchPratices.java
@@ -2,7 +2,7 @@ package Lectures;
 
 public class BinarySearchPratices {
     public static void main(String[] args){
-
+/*
         int[] arr = {1, 2, 2, 2, 2, 2, 2, 3, 3, 7, 7, 7, 8, 9, 9, 32};
 
 
@@ -25,6 +25,35 @@ public class BinarySearchPratices {
             }
         }
         System.out.println("Last Occurrence of " + target + " is at index: " + answer);
+
+
+ */
+
+//minimum element in a Rotated Sorted Array with duplicates:
+        int[] arr = {3, 3, 7, 7, 7, 9, 9, 1, 2, 2, 2, 2, 2, 2};
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] > arr[end]) {
+                start = mid + 1;
+            }
+            else if (arr[mid] < arr[end]) {
+                end = mid;
+            }
+            else {
+                end--;
+            }
+        }
+
+        System.out.println("Minimum Element: " + arr[start]);
+        System.out.println("Index: " + start);
+
+
+
 
 
 
