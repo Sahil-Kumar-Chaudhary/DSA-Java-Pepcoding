5a7e01a8456ccb01d3055c6fe4ae193f5361f13d
Sahil Kumar Chaudhary <sahilku707@gmail.com>
2026-07-01 09:15:00 +0530

    feat: add linked list basics

---
+++ src/Lectures/Linkedlist.java
@@ -0,0 +1,38 @@
+package Lectures;
+
+class Node{
+    int data;
+    Node next; 
+    Node(int val){
+        data = val;
+        next = null;
+    }
+}
+
+public class Linkedlist {
+    public static void main(String[] args){
+
+        Node a = new Node(10);
+        Node b = new Node(20);
+        Node c = new Node(30);
+        Node d = new Node(40);
+        Node e = new Node(50);
+        Node f = new Node(60);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = f;
+
+
+        System.out.println(a);
+        System.out.println(a.next.data);
+        System.out.println(b);
+        System.out.println(c.next);
+        System.out.println(d.next);
+        System.out.println(e.next);
+
+
+    }
+    
+}
