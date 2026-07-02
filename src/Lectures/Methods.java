995047d93c073b842a269c3b3f70e846d4fda02c
Sahil Kumar Chaudhary <sahilku707@gmail.com>
2026-07-02 13:00:00 +0530

    Add basics of methods and introductory notes

--- src/Lectures/Methods.java
+++ src/Lectures/Methods.java
@@ -1,27 +1,38 @@
 package Lectures;
 
+import java.util.*;
+
 public class Methods {
-    public static void main(String[] args){
 
-        /**
-         * The main method demonstrating various built-in Math class methods in Java.
-         * These methods include sqrt, cbrt, abs, floor, min, max, round, and pow.
-         */
+    public static void print(){
+        System.out.println("Hello World!");
+        for(int i=0;i<5;i++){
+            System.out.print(i+" ");
+        }
+        System.out.print("\n");
+    }
 
-        // Math Methods
-        System.out.println(Math.sqrt(10));
-        System.out.println(Math.cbrt(125));
-        System.out.println(Math.abs(-125));
-        System.out.println(Math.floor(-1.25));
-        System.out.println(Math.min(9,34));
-        System.out.println(Math.max(95,45));
-        System.out.println(Math.round(1.65));
-        //Find max of four numbers with built in method
-        System.out.println(Math.max(Math.max(2,3),Math.max(4,5)));
-        System.out.println(Math.pow(2,3));
-        System.out.println(Math.pow(0,0));//error it should not be 1.
-        
+    public static void sum(int a,int b){ // a and b are parameters
+        System.out.println(a+b);
 
     }
-    
+
+    public static int maximum(int x,int y){
+        System.out.print("Hello ");
+        System.out.println("User ");
+        return Math.max(x,y);
+
+    }
+
+    public static int check(int a){
+        if(a>10){
+            return 10;                   // it is mandorty to write else case in int method
+        }
+        else{
+            return 9;
+        }
+    }
+
+    public static void main(String[] args){
+    }
 }
