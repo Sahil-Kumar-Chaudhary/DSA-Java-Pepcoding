78e435b660b6c64d51e4bd74aa6604edc8da2835
Sahil Kumar Chaudhary <sahilku707@gmail.com>
2026-07-02 14:00:00 +0530

    Add factorial method and math methods notes

--- src/Lectures/Methods.java
+++ src/Lectures/Methods.java
@@ -33,6 +33,43 @@ public class Methods {
         }
     }
 
+    // Q -- Write a code for find permutations and combinations of n objects taken r at a time.
+
+    public static int fact(int x){
+        int a = 1;
+        for(int i=1;i<=x;i++){
+            a *=i;
+        }
+        return a;
+    }
+
+    
+
+
     public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        /*
+        // Math Methods
+        System.out.println(Math.sqrt(10));
+        System.out.println(Math.cbrt(125));
+        System.out.println(Math.abs(-125));
+        System.out.println(Math.floor(-1.25));
+        System.out.println(Math.min(9,34));
+        System.out.println(Math.max(95,45));
+        System.out.println(Math.round(1.65));
+        //Find max of four numbers with built in method
+        System.out.println(Math.max(Math.max(2,3),Math.max(4,5)));
+        System.out.println(Math.pow(2,3));
+        System.out.println(Math.pow(0,0));//error it should not be 1.
+        */
+
+        print();
+        sum(8,9); // 8 and 9 are argument
+        int x = maximum(3,9);
+        System.out.println(x);
+        System.out.println(maximum(12,6));
+
     }
 }
