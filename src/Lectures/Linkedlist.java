ee9be00a9b80ec899e000fb695b451197b6f49ee
Sahil Kumar Chaudhary <sahilku707@gmail.com>
2026-07-05 10:00:00 +0530

    feat: add Node class and basic Linlist methods (Part 1)

--- src/Lectures/Linkedlist.java
+++ src/Lectures/Linkedlist.java
@@ -5,34 +5,28 @@ class Node{
     Node next; 
     Node(int val){
         data = val;
-        next = null;
     }
 }
+class Linlist{
+        Node head;
+        Node tail;
 
-public class Linkedlist {
-    public static void main(String[] args){
-
-        Node a = new Node(10);
-        Node b = new Node(20);
-        Node c = new Node(30);
-        Node d = new Node(40);
-        Node e = new Node(50);
-        Node f = new Node(60);
-        a.next = b;
-        b.next = c;
-        c.next = d;
-        d.next = e;
-        e.next = f;
-
-
-        System.out.println(a);
-        System.out.println(a.next.data);
-        System.out.println(b);
-        System.out.println(c.next);
-        System.out.println(d.next);
-        System.out.println(e.next);
-
-
-    }
-    
+        void addatend(int x){
+            Node temp = new Node(x);
+            if(tail==null){
+                head=tail=temp;
+            }else{
+                tail.next = temp;
+                tail = temp;
+            }
+        }
+        void addathead(int y){
+            Node temp = new Node(y);
+            if(head == null){
+                head = tail = temp;
+            }else{
+                temp.next = head;
+                head = temp;
+            }
+        }
 }
