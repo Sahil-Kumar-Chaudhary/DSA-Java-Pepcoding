ecf734dc2957a949ea11872579fa9c5176e8f6ea
Sahil Kumar Chaudhary <sahilku707@gmail.com>
2026-07-06 14:00:00 +0530

    feat: add main method to Linkedlist with tests (Part 4)

--- src/Lectures/Linkedlist.java
+++ src/Lectures/Linkedlist.java
@@ -67,4 +67,44 @@ public class Linkedlist {
         System.out.print(head.data+" ");
         
     }
+
+    
+
+    public static void main(String[] args){
+        Linlist ll = new Linlist();
+
+        Node a = new Node(10);
+        Node b = new Node(20);
+        Node c = new Node(30);
+        Node d = new Node(40);
+        Node e = new Node(50);
+        Node f = new Node(60);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = f;
+
+
+        // System.out.println(a);
+        // System.out.println(a.next.data);
+        // System.out.println(b);
+        // System.out.println(c.next);
+        // System.out.println(d.next);
+        // System.out.println(e.next);
+        // display(a);
+        // reversell(a);
+        ll.addatend(70);
+        ll.addatend(80);
+        ll.addatend(90);
+        ll.addatend(100);
+        ll.display();
+        System.out.print("\n");
+        ll.addathead(65);
+        ll.display();
+
+
+
+    }
+    
 }
